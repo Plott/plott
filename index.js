98bcdce1046a43ca3b6624707fdbbe61853fa405
Corey White <smortopahri@gmail.com>
2015-08-26 21:00:18 -0400

    Added qualityDistance

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
  *Plott is an open-source project written in JavaScript working to create a modular high accuracy indoor positioning system
  *using WiFi fingerprinting. The goal of Plott is to create an API that is capable of tracking a targeted device within 1 foot
  *of its actual location and easily integrate with existing JavaScript web mapping libraries via GeoJson. In addition to the
- *technical goals of Plott, the project is being developed to preform geospatial analysis on K9 Nose Work training techniques. 
+ *technical goals of Plott, the project is being developed to preform geospatial analysis on K9 Nose Work training techniques.
  *
  * @module plott
  * @summary Indoor Positioning API
  */
 module.exports = {
-  rssiQuality: require('plott-rssi-quality')
+  rssiQuality: require('plott-rssi-quality'),
+  qualityDistance: require('plott-quality-distance')
 };
